af3869b7436478c42ec06b6a01edc96cbb593133
Mohamed Ahmed <mohamed.uwk2200@gmail.com>
2026-05-15 23:22:44 +0300

    Deploying

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
+ <BrowserRouter basename="/Company-Simple-Website">
     <Routes>
       <Route path="/" element={<RevolutionMediaLandingPage />} />
       <Route path="/login" element={<LoginPage />} />
